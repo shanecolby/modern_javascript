014f5ec963458ce52a1d14a5238976153e4adfe0
shane <shanecolbyzero8@gmail.com>
2020-12-09 19:17:51 -0600

    add webpack

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var moment = require('moment');
+
 console.log("this is the js file");
+
+console.log(moment().format('dddd'));
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+import { sqrt } from 'mathjs';
+
+console.log(sqrt(-4).toString()); // 2i
